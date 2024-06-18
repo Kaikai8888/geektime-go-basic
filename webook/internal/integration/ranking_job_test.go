5b7657667a0bdc7e4eb0ebf758a5430aedb266a6
Kelly Wu <kelly.wu@shoplineapp.com>
2024-06-18 21:29:42 +0800

    test: update test setup

--- webook/internal/integration/ranking_job_test.go
+++ webook/internal/integration/ranking_job_test.go
@@ -6,6 +6,7 @@ import (
 
 	"gitee.com/geekbang/basic-go/webook/internal/integration/startup"
 	"gitee.com/geekbang/basic-go/webook/internal/job"
+	"gitee.com/geekbang/basic-go/webook/internal/repository/cache"
 	svcmocks "gitee.com/geekbang/basic-go/webook/internal/service/mocks"
 	"gitee.com/geekbang/basic-go/webook/pkg/logger"
 	rlock "github.com/gotomicro/redis-lock"
@@ -13,6 +14,9 @@ import (
 	"github.com/robfig/cron/v3"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/mock/gomock"
+
+	repostiory "gitee.com/geekbang/basic-go/webook/internal/repository"
+	"gitee.com/geekbang/basic-go/webook/internal/service"
 )
 
 // TestRankingJob 这个测试只是测试调度和 Redis 交互两个部分，但是不会真的测试计算的逻辑
@@ -21,10 +25,13 @@ func TestRankingJob(t *testing.T) {
 	defer ctrl.Finish()
 	rdb := startup.InitRedis()
 	svc := svcmocks.NewMockRankingService(ctrl)
-	load :=
-		// 会调用三次
-		svc.EXPECT().RankTopN(gomock.Any()).Times(3).Return(nil)
-	j := job.NewRankingJob(svc, rlock.NewClient(rdb),
+	l := logger.NewNoOpLogger()
+	loadCache := cache.NewRedisLoadCache(rdb, l)
+	loadRepo := repostiory.NewLoadRepository(loadCache)
+	loadSvc := service.NewLoadService(loadRepo, l)
+	// 会调用三次
+	svc.EXPECT().RankTopN(gomock.Any()).Times(3).Return(nil)
+	j := job.NewRankingJob(svc, rlock.NewClient(rdb), loadSvc, "mock-instance-id",
 		logger.NewNoOpLogger(), time.Minute)
 	c := cron.New(cron.WithSeconds())
 	bd := job.NewCronJobBuilder(logger.NewNoOpLogger(),
